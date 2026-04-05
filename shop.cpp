cf0e9ba76a9e3d73b099f67af14c9254e30b5d7d
“Nuhaa <nuhaaajamu@gmail.com>
2026-04-05 16:19:37 -0400

    Implemented main logic for merge using a helper function

--- shop.cpp
+++ shop.cpp
@@ -38,26 +38,96 @@ Shop& Shop::operator=(const Shop& rhs) {
 
 }
 
+
+Order* Shop::mergeOperation(Order* h1, Order* h2) {
+  // Base Case
+  if (h1 == nullptr) {
+    return h2;
+  }
+  if (h2 == nullptr) {
+    return h1;
+  }
+
+  Order* higherPrior = nullptr;
+  Order* lowerPrior = nullptr;
+
+  int h1Prior = m_priorFunc(*h1);
+  int h2Prior = m_priorFunc(*h2);
+
+  // Smaller value is higher priority.
+  if (m_heapType == MINHEAP) {
+    // Compare roots to determine node with the higher priority.
+    if (h1Prior <= h2Prior) {
+      higherPrior = h1;
+      lowerPrior = h2;
+    }else {
+      higherPrior = h2;
+      lowerPrior = h1;
+    }
+  }
+
+  // Larger value is higher priority.
+  if (m_heapType == MAXHEAP) {
+    // Compare roots to determine node with the higher priority.
+    if (h1Prior >= h2Prior) {
+      higherPrior = h1;
+      lowerPrior = h2;
+    }else {
+      higherPrior = h2;
+      lowerPrior = h1;
+    }
+  }
+
+  // Store right and left children of node with higher priority for easier reference.
+  Order* right = higherPrior->m_right;
+  Order* left = higherPrior->m_left;
+
+  // Recursive call on higher priority node's right child.
+  mergeOperation(right, lowerPrior);
+
+  // Swap the children of the higher priority node for a skew heap. This is done every recursive call.
+  if (m_structure == SKEW) {
+    higherPrior->m_right = left;
+    higherPrior->m_left = right;
+  }
+
+  // Swap the children of the higher priority node for a leftist heap.
+  // This is only done if the right subtree is heavier than the left.
+  if (m_structure == LEFTIST) {
+    if (left->m_npl < right->m_npl) {
+      higherPrior->m_right = left;
+      higherPrior->m_left = right;
+    }
+  }
+
+  // Return the root.
+  return higherPrior;
+}
+
+
 void Shop::mergeWithQueue(Shop& rhs) {
 
 }
 
+// Must use merge.
 bool Shop::insertOrder(const Order& order) {
 
 }
 
 int Shop::numOrders() const{return m_size;}
-
 prifn_t Shop::getPriorityFn() const {return m_priorFunc;}
 
+// Must use merge.
 Order Shop::getNextOrder() {
 
 }
 
+// Must rebuild.
 void Shop::setPriorityFn(prifn_t priFn, HEAPTYPE heapType) {
 
 }
 
+// Must rebuild.
 void Shop::setStructure(STRUCTURE structure){
 
 }
@@ -66,6 +136,7 @@ HEAPTYPE Shop::getHeapType() const {return m_heapType;}
 
 STRUCTURE Shop::getStructure() const {return m_structure;}
 
+// Use preorder traversal.
 void Shop::printOrdersQueue() const {
 
 }
